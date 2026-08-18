c506875c666f75c97002ddf07c2f334835f5adca
Ege Cantürk <canturkege74@gmail.com>
2026-08-18 18:29:18 +0300

    Update config.js

--- dnd/config.js
+++ dnd/config.js
@@ -1,1 +1,1 @@
-window.DM_CONFIG={SUPABASE_URL:'https://mfzytjjwiwsezjunwmhc.supabase.co',SUPABASE_ANON_KEY:'sb_publishable_CyoOE3zloOcbFnSI_hMPsA_VBYXywNc'};
+window.DM_CONFIG={SUPABASE_URL:'https://okxtjzyudhsvenetpmin.supabase.co',SUPABASE_ANON_KEY:'sb_publishable_O1WNt6PS67CfQ1TAsnPEXw_6TmlBtVz'};
